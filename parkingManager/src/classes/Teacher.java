3d9b21870cb97ef0897bd54c4a273269bc48b703
EduardoPEMA <eduardopro00@gmail.com>
2020-05-01 18:58:20 -0500

    Revert "Revert "update JFrame""
    This reverts commit ac09fb49c9efe8df038966c68fe7fd7fcd249cd1.

---
+++ parkingManager/src/classes/Teacher.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author atlas
+ */
+public class Teacher extends Person{
+    String department = "";
+    
+    
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+    
+    
+    
+    
+}
